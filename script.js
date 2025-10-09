9c506319ff1052fb8a34f30c4d5993eceba1d160
trplayerdomain <moisoft503@gmail.com>
2025-10-09 20:01:20 +0300

    init commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var oldDomains = ["airnols.net","rollbun.com","ultorz.store"],
-	newDomains = ["klogdik.site"],
+var oldDomains = ["airnols.net","rollbun.com","klogdik.site"],
+	newDomains = ["ultorz.store"],
 		delay = 200;
 
 if (oldDomains.length && newDomains.length) {
